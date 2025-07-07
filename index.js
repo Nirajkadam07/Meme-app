0d5d2914c4166e0838094da0aa7fa9eb23dde244
Nirajkadam07 <64134180+Nirajkadam07@users.noreply.github.com>
2025-07-07 23:54:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+import { catsData } from "./data.js";
+
+const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
+
+emotionRadios.addEventListener("change", highlightCheckedOption);
+
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
+function highlightCheckedOption(e) {
+  const radiosArr = document.getElementsByClassName("radio");
+  for (let radio of radiosArr) {
+    radio.classList.remove("highlight");
+  }
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
+}
+
+function getMatchingCatsArray() {
+  if (document.querySelector("input[type=radio]:checked")) {
+    const selectedEmotion = document.querySelector(
+      "input[type=radio]:checked"
+    ).value;
+    console.log(selectedEmotion);
+    const isGif = gifsOnlyOption.checked;
+
+    const matchingCatsArray = catsData.filter(function (cat) {
+      return cat.emotionTags.includes(selectedEmotion);
+    });
+    console.log(matchingCatsArray);
+  }
+}
+
+function getEmotionsArray(cats) {
+  //   for (let i = 0; i < cats.length; i++) {
+  //     for (let j = 0; j < cats[i].emotionTags.length; j++) {
+  //       emotionsArray.push(cats[i].emotionTags[j]);
+  //     }
+  //   }
+
+  const emotionsArray = [];
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
+    }
+  }
+  return emotionsArray;
+}
+
+function renderEmotionsRadios(cats) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(cats);
+
+  // for (let i = 0; i < emotions.length; i++) {
+  //   radioItems += `<p>${emotions[i]}</p>`;
+  // }
+
+  for (let emotion of emotions) {
+    radioItems += `
+      <div class="radio">
+        <label for='${emotion}'>${emotion}</label>
+        <input type='radio' id='${emotion}' value='${emotion}' name='emotions'>
+      </div>
+    `;
+  }
+  emotionRadios.innerHTML = radioItems;
+}
+
+renderEmotionsRadios(catsData);
